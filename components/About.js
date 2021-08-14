98e7e4503ba26cb797ed7d16e4e38fa02f4ad212
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-08-14 15:37:55 +0530

    about

--- components/About.js
+++ components/About.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 import { useRouter } from "next/router";
 
-import styles from '../styles/About.module.css'
+import styles from '../styles/About.module.css';
+
+import aboutImg from '../assets/svg/about.svg'
 
 function About() {
 
@@ -10,7 +13,16 @@ function About() {
 
     return (
         <div className={styles.about} id="about">
-            <h1>ABOUT</h1>
+            <div className={styles.aboutBody}>
+                <div className={styles.aboutDescription}>
+                    <h2>About</h2>
+                    <h1>Advantus Life Sciences</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id fermentum at dignissim ornare amet. Nullam nunc quam amet morbi blandit purus. Ultricies sit urna vitae purus. Et cursus in euismod vel ultrices feugiat volutpat vitae, aliquam.</p>
+                </div>
+                <div className={styles.aboutImage}>
+                    <Image src={aboutImg} layout="responsive" height={500} width={1400} alt=""/>
+                </div>
+            </div>
         </div>
     )
 }
