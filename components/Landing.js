fd1a7d8d7a60b1d15bbfc20e34ed5f1fce5ad17a
Hemanth R <hemanthrt18@gmail.com>
2022-03-10 20:43:59 +0530

    .

--- components/Landing.js
+++ components/Landing.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link'
-
 import styles from '../styles/Landing.module.css';
 
 import img1 from '../assets/svg/landingPage/img1.svg';
